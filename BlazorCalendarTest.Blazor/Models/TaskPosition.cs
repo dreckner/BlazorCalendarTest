d731827544989c299b02aef5e1c21c26c6309524
David Reckner <dreckner@reckner.com>
2024-08-02 08:01:31 -0400

    before feedback

--- BlazorCalendarTest.Blazor/Models/TaskPosition.cs
+++
@@ -1,9 +0,0 @@
-namespace BlazorCalendarTest.Blazor.Models;
-
-internal sealed class TaskPosition
-{
-    public int Counter { get; set; }
-    public bool Top { get; set; }
-    public bool Center { get; set; }
-    public bool Bottom { get; set; }
-}
